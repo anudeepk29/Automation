0c7f4785d17f99e9ac217a21da29c7cdf9c349e2
root <root@ssc-vm-rhev4-3208.colo.seagate.com>
2022-12-08 22:06:37 -0700

    correcting all errors

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,22 +1,13 @@
-terraform {
-
-
-
   provider "aws" {
     region = "us-east-1"
-
   }
 
   resource "aws_instance" "app_server" {
-    ami = "ami-0b0dcb5067f052a63"
+    ami           = "ami-0b0dcb5067f052a63"
     instance_type = "t2.micro"
 
     tags = {
-        name = "MyServer"
+      name = "MyServer"
     }
 
   }
-}
-
-
-}
