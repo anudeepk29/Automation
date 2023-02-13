d49bd5fb51fbc70bf3e1bc604f6f001c58d70d95
anudeepk29 <46772745+anudeepk29@users.noreply.github.com>
2023-02-13 11:02:06 -0500

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16 > kubeout
 
 sleep 120
 
 kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 
 alias k=kubectl
+
+token=`cat kubeout | grep "kubeadm join"
+
+echo $token
