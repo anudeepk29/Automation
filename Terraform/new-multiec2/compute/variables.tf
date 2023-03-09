27a77ff4827ab6b723e16294a3e5be6bae6e8074
anudeepk29 <anudeep29@gmail.com>
2023-03-09 00:36:37 -0600

    adding the modulraization to new-multiec2

---
+++ Terraform/new-multiec2/compute/variables.tf
@@ -0,0 +1,5 @@
+variable "ec2_ami" { }
+variable "server_tag" {	}
+variable "ec2_key" { } 
+variable "ec2_count" { }
+variable "ec2_sg" { }
---
+++ Terraform/new-multiec2/k8s-workers/main.tf
@@ -0,0 +1,9 @@
+module "k8s-workers" {
+	source = "./../compute"
+	ec2_ami = "ami-0c9978668f8d55984" 
+	server_tag = "k8s"
+	ec2_key = "kp3"
+	ec2_count  = "1"
+	ec2_sg = ["launch-wizard-1"]
+
+}
