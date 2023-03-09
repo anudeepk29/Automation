380ba09cf0b8c81bc4e29b5d36a331295a0bfbcf
anudeepk29 <anudeep29@gmail.com>
2023-03-09 00:47:47 -0600

    new Folders for k8s-master and jenkins-master

--- Terraform/new-multiec2/jenkins/main.tf
+++ Terraform/new-multiec2/jenkins/main.tf
@@ -1,7 +1,7 @@
-module "k8s-workers" {
+module "jenkins-master" {
 	source = "./../compute"
 	ec2_ami = "ami-0c9978668f8d55984" 
-	server_tag = "k8s-worker"
+	server_tag = "jenkins-master"
 	ec2_key = "kp3"
 	ec2_count  = "1"
 	ec2_sg = ["launch-wizard-1"]
