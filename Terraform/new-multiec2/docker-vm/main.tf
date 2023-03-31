66912957366b1df521b4d3d7005d9240d75a04a4
anudeepk29 <anudeep29@gmail.com>
2023-03-30 22:54:52 -0500

    adding ansible playbooks for docker and some cleanup

--- Terraform/new-multiec2/docker-vm/main.tf
+++ Terraform/new-multiec2/docker-vm/main.tf
@@ -1,9 +1,10 @@
-module "jenkins-master" {
+module "docker-vm" {
 	source = "./../compute"
 	ec2_ami = "ami-0c9978668f8d55984" 
-	server_tag = "jenkins-master"
+	server_tag = "docker-vm"
 	ec2_key = "kp5"
 	ec2_count  = "1"
 	ec2_sg = ["launch-wizard-1"]
-	instance_type="t2.micro"
+	instance_type = "t2.micro"
+
 }
